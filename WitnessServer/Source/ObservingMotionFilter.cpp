0d76c4384be87d11edb1ee70c579ad3f81972003
Ian Norris <icstatic@gmail.com>
2018-07-10 22:08:41 +0100

    Moved to self built opencv binary (4.0.0) so we can make use of opencv_contrib as well.

--- WitnessServer/Source/ObservingMotionFilter.cpp
+++ WitnessServer/Source/ObservingMotionFilter.cpp
@@ -3,7 +3,10 @@
 #include "MessageBus.h"
 
 #include <opencv2/imgcodecs.hpp>
+#include <opencv2/imgcodecs/imgcodecs_c.h>
+#include <opencv2/core/core_c.h>
 #include <opencv2/imgproc.hpp>
+#include <opencv2/imgproc/imgproc_c.h>
 
 const int ClipEndGracePeriodInSeconds = 10;
 const int TargetThumbnailSize = 400;
