3a9ed8c70583020af39ce6104944803e8e894066
Ian Norris <icstatic@gmail.com>
2019-07-16 00:55:34 +0100

    Release script and gitignore.

--- PrepareReleaseFolder.bat
+++ PrepareReleaseFolder.bat
@@ -24,3 +24,4 @@ rmdir x64\Release\x86 /S /Q
 mkdir x64\Release\Web
 
 robocopy /njh /njs /np /nfl /ndl /S WitnessServer\Web x64\Release\Web\
+robocopy /njh /njs /np /nfl /ndl /S ThirdParty\WACS x64\Release\WACS\
